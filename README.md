f024c7d1b2b5453d843cbf014d3b81f5a920c78f
starbor <cfb4356@163.com>
2022-01-16 01:38:26 +0800

    fix: init shell

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 ## One Click
 When you want to initialize development environment at macOS for developing web project, just use one click.
 
-## How to use
+## How to 
+- Firstly, if you never install git, so you can install the shell.sh file or create a new .sh file (PS: You can copy the code).
+- Then, just execute terminal commands below 
+
+```
+cd workspace
+
+chmod +x ./shell.sh
+```
---
+++ shell.sh
@@ -0,0 +1,45 @@
+# confirm system type
+sys=`uname -a`
+
+mac="Darwin"
+linux="linux"
+
+install() {
+    # install homebrew
+    #git clone https://github.com/Homebrew/brew homebrew
+
+    # eval "$(homebrew/bin/brew shellenv)"
+    # brew update --force --quiet
+    # chmod -R go-w "$(brew --prefix)/share/zsh"
+
+    # install nvm
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+    checkNVM=`command -v nvm`
+    
+    # install node
+    if [[$checkNVM = "nvm"]]
+        nvm install --lts
+    elif
+        chmod +x ~/.nvm/nvm.sh
+        nvm install --lts
+    fi
+
+    nvm use --lts
+    
+    # install omz
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
+
+
+if [[ $sys =~ $mac ]]; then
+    xcode-install 
+elif [[ $sys =~ $linux ]]; then
+    yum install -y git
+
+    # execute
+    install
+else
+    echo "your laptop OS is $sys"
+fi
+
